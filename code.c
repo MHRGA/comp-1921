a942e3b021abbf6d12f5c49e3091fa0abcddbae3
Muhamad Hewa Rahim <146334970+MHRGA@users.noreply.github.com>
2024-02-13 12:32:39 +0000

    Started Skeleton Code

--- code.c
+++ code.c
@@ -1,4 +1,27 @@
-int main() 
-{
+typedef struct {
+    int width;
+    int height;
+    int start;
+    int end;
+    PLAYER character;
+} MAZE;
+
+typedef struct {
+    char player;
+} PLAYER;
+
+int move_character() {
+
+}
+
+int read_from_file() {
+    // error checking to make sure file exists
+
+}
+
+int main() {
+    // ask user for filename
+    // call function to read file
+    // ask user for input
     return 0;
 }
