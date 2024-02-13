a833f5053d1f2becd21b64ad156fbb04ed98dcf3
Muhamad Hewa Rahim <146334970+MHRGA@users.noreply.github.com>
2024-02-13 15:07:37 +0000

    Started Skeleton Code

--- code.c
+++ code.c
@@ -7,21 +7,45 @@ typedef struct {
 } MAZE;
 
 typedef struct {
-    char player;
+    int position;
 } PLAYER;
 
-int move_character() {
-
+int check_cell(int new_position, MAZE maze) {
+    // check if new space is not wall
+    // if wall, prompt user and return 1
+    // check if new space is end position (goal)
+    // if end, prompt user they won and exit program
+    // else return 0
 }
 
-int read_from_file() {
-    // error checking to make sure file exists
+int open_map(MAZE maze) {
+    // print 2d array to user
+}
 
+int move_character(PLAYER player, char *map) {
+    // call check cell function
+    // if check cell returns 1, exit this function
+    // update struct details
+    // update 2d array map
+}
+
+int read_from_file(int input) {
+    // error checking to make sure file exists
+    // if file doesnt exist, throw error and exit program
+    // open file
+    // copy contents of file into struct
+    // check if file is valid (width and height meet requirements)
+    // if not, throw error and exit program
 }
 
 int main() {
-    // ask user for filename
-    // call function to read file
-    // ask user for input
+    // store command line argument as filename
+    // call function read_from_file
+    // show map to user
+    // start user at start position
+    // infinite while loop
+        // ask user for input
+        // switch statement for moving in four directions or opening map
+        // call relevent function (move_character for WASD and open_map for M)
     return 0;
 }
